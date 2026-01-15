9fb9d8374048e0a4f114a95a6072212794d053d7
Nithish Karthick <nithishnickzz@gmail.com>
2026-01-15 12:38:51 +0530

    Switched database from H2 to PostgreSQL

--- src/main/java/com/ltp/gradesubmission/security/SecurityConfig.java
+++ src/main/java/com/ltp/gradesubmission/security/SecurityConfig.java
@@ -28,11 +28,11 @@ public class SecurityConfig {
         AuthenticationFilter authenticationFilter = new AuthenticationFilter(customAuthenticationManager);
         authenticationFilter.setFilterProcessesUrl("/authenticate");
         http        
-            .headers().frameOptions().disable() // New Line: the h2 console runs on a "frame". By default, Spring Security prevents rendering within an iframe. This line disables its prevention.
-            .and()
+           // .headers().frameOptions().disable() // New Line: the h2 console runs on a "frame". By default, Spring Security prevents rendering within an iframe. This line disables its prevention.
+           // .and()
             .csrf().disable()
             .authorizeRequests()  
-            .antMatchers("/h2/**").permitAll() // New Line: allows us to access the h2 console without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /h2.
+           // .antMatchers("/h2/**").permitAll() // New Line: allows us to access the h2 console without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /h2.
             .antMatchers(HttpMethod.POST, SecurityConstants.REGISTER_PATH).permitAll()
             .anyRequest().authenticated()
             .and()
